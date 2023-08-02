57c2258e65875a029a81c24adbbeaa3020ce5e3a
Axel Cymerman <axelcy@gmail.com>
2023-08-02 08:59:24 -0300

    Routes, Layout, Navbar

---
+++ src/components/Layout.jsx
@@ -0,0 +1,27 @@
+import { Link, Outlet } from "react-router-dom"
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import './Layout.css'
+
+function Layout() {
+    return (
+        <>
+            <Navbar expand="lg" className="bg-body-tertiary layout-container">
+                <Container>
+                    <Link className="brand" to={'/'}>Cambio climático</Link>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto navbar-links">
+                            <Link to={'/'}>Home</Link>
+                            <Link to={'/test1234'}>Test1234</Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+
+
+            <Outlet />
+        </>
+    )
+}
+
+export default Layout
